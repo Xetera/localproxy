3c74b159f7683b01784d9af2ad1a46984f441544
Xetera <contact@xetera.dev>
2026-01-26 12:12:35 +0300

    fix envoy in devenv with derivation

--- devenv.nix
+++ devenv.nix
@@ -1,10 +1,17 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
+let envoy = import ./envoy.nix { inherit pkgs lib; stdenv = pkgs.stdenv; };
+in
 {
-  env.CGO_ENABLED = 0 ;
+  env.CGO_ENABLED = 0;
   packages = [
     pkgs.git
     pkgs.mkcert
+    envoy
   ];
+  enterTest = ''
+    envoy --version | grep envoy
+  '';
+
   languages.go.enable = true;
 }
